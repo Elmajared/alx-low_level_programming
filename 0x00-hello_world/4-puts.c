6b7e9ff33c75c80341f5664dba78dbf8bb0a398d
Damilola Elijah <Damilolaelijah8@gmail.com>
2022-10-13 16:37:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Prints string
- *
- *   * Description: Prints "\"Programming is like building a multilingual puzzle"
- *
- *    * Return: 0
- *
- *     */
-
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
